749693db726732fd88f361c9af1ddff14fe51ceb
paoloeric50-ux <paoloeric50@gmail.com>
2026-03-20 05:40:24 +0800

    Revert "OOP"
    This reverts commit 0f1a5766d0f807f6eb2357991ef849a6b5b82c91.

--- backend-java/src/main/java/com/motorph/payroll/dto/DeductionCalculateRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.motorph.payroll.dto;
-
-public class DeductionCalculateRequestDto {
-    private double grossSalary;
-
-    public double getGrossSalary() { return grossSalary; }
-    public void setGrossSalary(double grossSalary) { this.grossSalary = grossSalary; }
-}
